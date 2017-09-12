cf107b6b635605930ad54fbbc4057f01f91091ad
Olzhas Kaiyrakhmet <olzhabay.i@gmail.com>
2017-09-12 14:54:40 +0900

    nvskiplist template created

--- memtable/nv_skiplist.cc
+++ memtable/nv_skiplist.cc
@@ -11,4 +11,43 @@ MemTableRep* NVSkipListFactory::CreateMemTableRep(
   return new NVSkipListRep(compare, allocator, transform, lookahead_);
 }
 
+KeyHandle NVSkipListRep::Allocate(const size_t len, char **buf) {
+  return MemTableRep::Allocate(len, buf);
+}
+
+void NVSkipListRep::Insert(KeyHandle handle) {
+
+}
+
+void NVSkipListRep::InsertWithHint(KeyHandle handle, void **hint) {
+  MemTableRep::InsertWithHint(handle, hint);
+}
+
+void NVSkipListRep::InsertConcurrently(KeyHandle handle) {
+  MemTableRep::InsertConcurrently(handle);
+}
+
+void NVSkipListRep::MarkReadOnly() {
+  MemTableRep::MarkReadOnly();
+}
+
+void NVSkipListRep::Get(const LookupKey &k, void *callback_args, bool (*callback_func)(void *, const char *)) {
+  MemTableRep::Get(k, callback_args, callback_func);
+}
+
+uint64_t NVSkipListRep::ApproximateNumEntries(const Slice &start_ikey, const Slice &end_key) {
+  return MemTableRep::ApproximateNumEntries(start_ikey, end_key);
+}
+
+size_t NVSkipListRep::ApproximateMemoryUsage() {
+  return 0;
+}
+
+NVSkipListRep::Iterator *NVSkipListRep::GetIterator(Arena *arena) {
+  return nullptr;
+}
+
+bool NVSkipListRep::Contains(const char *key) const {
+  return false;
+}
 } // namespace rocksdb
--- memtable/nv_skiplist.h
+++ memtable/nv_skiplist.h
@@ -12,13 +12,60 @@ class NVSkipListRep : public MemTableRep {
  public:
   explicit NVSkipListRep(const MemTableRep::KeyComparator& compare,
                          Allocator* allocator, const SliceTransform* transform,
-                         const size_t lookahead) 
+                         const size_t lookahead)
   : MemTableRep(allocator),
     cmp_(compare),
     transform_(transform),
     lookahead_(lookahead) {}
-  
-  
+
+  virtual KeyHandle Allocate(const size_t len, char** buf);
+
+  virtual void Insert(KeyHandle handle);
+
+  virtual void InsertWithHint(KeyHandle handle, void** hint);
+
+  virtual void InsertConcurrently(KeyHandle handle);
+
+  virtual bool Contains(const char* key) const;
+
+  virtual void MarkReadOnly();
+
+  virtual void Get(const LookupKey& k, void* callback_args,
+                   bool (*callback_func)(void* arg, const char* entry));
+
+  virtual uint64_t ApproximateNumEntries(const Slice& start_ikey,
+                                         const Slice& end_key);
+
+  virtual size_t ApproximateMemoryUsage();
+
+  virtual ~NVSkipListRep() override {}
+
+  class Iterator : public MemTableRep::Iterator{
+   public:
+    virtual ~Iterator() {}
+
+    virtual bool Valid() const = 0;
+
+    virtual const char* key() const = 0;
+
+    virtual void Next() = 0;
+
+    virtual void Prev() = 0;
+
+    virtual void Seek(const Slice& internal_key, const char* memtable_key) = 0;
+
+    virtual void SeekForPrev(const Slice& internal_key,
+                             const char* memtable_key) = 0;
+
+    virtual void SeekToFirst() = 0;
+
+    virtual void SeekToLast() = 0;
+  };
+
+  virtual Iterator* GetIterator(Arena* arena = nullptr);
+
+
+
 };
 
 
